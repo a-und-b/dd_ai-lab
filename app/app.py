2e854e98d632ec65eefd7ba0a007651c6159ece8
Holger Will <holger@andersundbesser.de>
2024-05-01 08:13:27 +0200

    feature: create job folder structure at check-in

--- app/app.py
+++ app/app.py
@@ -1,27 +1,25 @@
-# app/app.py
 from flask import Flask, request, jsonify, render_template, Blueprint
 from .models import Job, Session, engine
+import os
 
 app = Blueprint('app', __name__)
+
+def create_job_directories(job_id):
+    base_path = f"jobs/{job_id}"
+    directories = [
+        base_path,
+        f"{base_path}/raw",
+        f"{base_path}/controlnet_assets",
+        f"{base_path}/ipadapter",
+        f"{base_path}/masks",
+        f"{base_path}/generated_images"
+    ]
+    for directory in directories:
+        os.makedirs(directory, exist_ok=True)
 
 @app.route('/')
 def home():
     return render_template('check_in.html')
-
-@app.route('/jobs', methods=['GET'])
-def get_jobs():
-    session = Session()
-    jobs = session.query(Job).all()
-    jobs_data = [
-        {
-            "id": job.id,
-            "nickname": job.nickname,
-            "status": job.status,
-            "created_at": job.created_at.strftime("%Y-%m-%d %H:%M:%S"),  # Formatieren des Datums
-            "job_url": job.job_url
-        } for job in jobs
-    ]
-    return jsonify(jobs_data)
 
 @app.route('/jobs', methods=['POST'])
 def create_job():
@@ -30,7 +28,23 @@ def create_job():
     new_job = Job(nickname=nickname)
     session.add(new_job)
     session.commit()
+
+    # Erstelle die Ordnerstruktur für den neuen Job
+    create_job_directories(new_job.id)
+
     return jsonify({"job_id": new_job.id, "job_url": new_job.job_url}), 201
+
+@app.route('/jobs', methods=['GET'])
+def get_jobs():
+    session = Session()
+    jobs = session.query(Job).all()
+    jobs_data = [{
+        "id": job.id,
+        "nickname": job.nickname,
+        "status": job.status,
+        "job_url": job.job_url
+    } for job in jobs]
+    return jsonify(jobs_data)
 
 @app.route('/jobs/<job_id>', methods=['PUT'])
 def update_job_status(job_id):
@@ -45,7 +59,6 @@ def update_job_status(job_id):
         else:
             return jsonify({"error": "Invalid status provided"}), 400
     return jsonify({"error": "Job not found"}), 404
-
 
 @app.route('/jobs/<job_id>', methods=['GET'])
 def get_job(job_id):
--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, Column, String, DateTime, Integer
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 from datetime import datetime
 from hashlib import sha256
 
